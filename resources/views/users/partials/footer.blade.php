2b6f752aa0c92085a7fe8c54a128e86f0c9039f6
Xuanngoc <xuanngoc10a@gmail.com>
2026-06-13 08:36:07 +0700

    update

--- resources/views/users/partials/footer.blade.php
+++ resources/views/users/partials/footer.blade.php
@@ -4,8 +4,9 @@
                 <div class="col-md-4 item_ft contact_ft">
                     <div class="title_ft">Liên hệ</div>
                     <div class="info_col">
+                        {{-- <p class="footer_slogan mb-3">Nơi biến ước mơ về ngày cưới của bạn thành sự thật. Hơn 5 năm đồng hành và kiến tạo hạnh phúc cho hàng nghìn cặp đôi.</p> --}}
                         <div class="item">
-                            <span>Chi nhánh </span>
+                            <span>Chi nhánh</span>
                             <div class="name">Địa chỉ: 152 Lê Trọng Tấn, Thanh Xuân, HN</div>
                         </div>
                         <div class="item">
@@ -18,113 +19,70 @@
                     <div class="title_ft">Hỗ trợ khách hàng</div>
                     <div class="info_col">
                         <a class="item d-flex text-white" href="mailto:anhvienmimosa@gmail.com">
-                        <img class="lazyload" data-src="https://mimosawedding.vn/wp-content/uploads/2023/05/Icon.svg" alt="">
-                        <div class="info_sp">
-                            <span>Email</span>
-                            <div class="name">anhvienmimosa@gmail.com</div>
-                        </div>
-                    </a>
+                            <img class="lazyload" data-src="https://mimosawedding.vn/wp-content/uploads/2023/05/Icon.svg" alt="">
+                            <div class="info_sp">
+                                {{-- <span>Email</span> --}}
+                                <div class="name">anhvienmimosa@gmail.com</div>
+                            </div>
+                        </a>
                         <a class="item d-flex text-white" href="tel:(+84)37 785 55 55">
-                        <img class="lazyload" data-src="https://mimosawedding.vn/wp-content/uploads/2023/05/Icon-1.svg" alt="">
-                        <div class="info_sp">
-                            <span>Complain</span>
-                            <div class="name">(+84)37 785 55 55</div>
-                        </div>
-                    </a>
+                            <img class="lazyload" data-src="https://mimosawedding.vn/wp-content/uploads/2023/05/Icon-1.svg" alt="">
+                            <div class="info_sp">
+                                {{-- <span>Complain</span> --}}
+                                <div class="name">(+84)37 785 55 55</div>
+                            </div>
+                        </a>
                         <a class="item d-flex text-white" href="tel:(+84)38 576 66 66">
-                        <img class="lazyload" data-src="https://mimosawedding.vn/wp-content/uploads/2023/05/call-calling.svg" alt="">
-                        <div class="info_sp">
-                            <span>Hotline</span>
-                            <div class="name">(+84)38 576 66 66</div>
-                        </div>
-                    </a>
+                            <img class="lazyload" data-src="https://mimosawedding.vn/wp-content/uploads/2023/05/call-calling.svg" alt="">
+                            <div class="info_sp">
+                                {{-- <span>Hotline</span> --}}
+                                <div class="name">(+84)38 576 66 66</div>
+                            </div>
+                        </a>
                     </div>
                 </div>
-                <div class="col-md-4 item_ft fanpage_ft">
-                    <div class="title_ft">Fanpage</div>
+                <div class="col-md-4 item_ft menu_ft">
+                    <div class="title_ft">Khám phá</div>
                     <div class="info_col">
-                        <!-- <div class="fb-page" data-href="https://www.facebook.com/anhvienmimosa" data-tabs="" data-width="400" data-height="400" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false"><blockquote cite="https://www.facebook.com/anhvienmimosa" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/anhvienmimosa">Sorabridal</a></blockquote></div> -->
-                        <!-- <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fanhvienmimosa&tabs&width=400&height=400&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId=2041254136266004" width="400" height="400" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe> -->
-                        <!-- <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fanhvienmimosa&tabs=timeline&width=340&height=271&small_header=true&adapt_container_width=false&hide_cover=false&show_facepile=true&appId=2041254136266004" width="100%" height="100%" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe> -->
-                        <div id="fb-root"></div>
-                        <script async defer crossorigin="anonymous" src="{{ asset('assets/mimosa/js/facebook-sdk-vi.js') }}#xfbml=1&version=v16.0&appId=2041254136266004&autoLogAppEvents=1" nonce="fXIGfJkS"></script>
-                        <div class="fb-page" data-href="https://www.facebook.com/anhvienmimosa/" data-tabs="timeline" data-width="500" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
-                            <blockquote cite="https://www.facebook.com/anhvienmimosa/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/anhvienmimosa/">Sorabridal</a></blockquote>
-                        </div>
-                        <!-- <div id="fb-root"></div>
-                    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v17.0&appId=954476962464958&autoLogAppEvents=1" nonce="qfIOQvub"></script>
-                    <div class="fb-page" data-href="https://www.facebook.com/anhvienmimosa" data-tabs="" data-width="" data-height="" data-small-header="true" data-adapt-container-width="false" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/anhvienmimosa" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/anhvienmimosa">Sorabridal</a></blockquote></div> -->
+                        <ul class="list-unstyled footer_menu m-0 p-0">
+                            <li><a href="/goi-chup-anh-cuoi/">Chụp ảnh cưới</a></li>
+                            <li><a href="/goi-chup-gia-dinh/">Chụp ảnh gia đình</a></li>
+                            <li><a href="/concept/">Concept cưới</a></li>
+                            <li><a href="/trang-diem/">Phóng sự</a></li>
+                            {{-- <li><a href="/vay-cuoi/">Trang phục</a></li>
+                            <li><a href="/khach-hang/">Trang điểm</a></li>
+                            <li><a href="/khoa-hoc/">Dạy nghề</a></li>
+                            <li><a href="/tin-tuc/">Tin tức</a></li> --}}
+                        </ul>
                     </div>
                 </div>
-                <div class="col-md-4 item_ft follow_ft">
+                <div class="col-md-12 item_ft follow_ft pb-5">
                     <div class="title_ft">Follow us</div>
-                    <div class="info_col">
-                        <a class="item d-flex" style="background:#1066E7" href="https://www.facebook.com/anhvienmimosa">
+                    <div class="info_col d-flex flex-wrap follow_ft__icons">
+                        <a class="item" style="background:#1066E7" href="https://www.facebook.com/sorabridal" target="_blank" rel="noopener" aria-label="Facebook">
                             <div class="icon"><i class="fa-brands fa-facebook-f"></i></div>
-                            <div class="info_sp">
-                                301k lượt thích </div>
                         </a>
-                        <a class="item d-flex" style="background:#000000" href="https://www.tiktok.com/@mimosa_wedding6133">
+                        <a class="item" style="background:#000000" href="https://www.tiktok.com/@sora.bridal.252" target="_blank" rel="noopener" aria-label="TikTok">
                             <div class="icon"><i class="fa-brands fa-tiktok"></i></div>
-                            <div class="info_sp">
-                                570k Follower </div>
                         </a>
-                        <a class="item d-flex" style="background:#890D0D" href="https://www.youtube.com/@MimosaWedding">
-                            <div class="icon"><i class="fa-brands fa-youtube"></i></div>
-                            <div class="info_sp">
-                                101k subscribe </div>
-                        </a>
-                        <a class="item d-flex" style="background:#CD486B" href="https://www.instagram.com/mimosa.wedding.studio/">
+                        <a class="item" style="background:#CD486B" href="https://www.instagram.com/sora.bridal/" target="_blank" rel="noopener" aria-label="Instagram">
                             <div class="icon"><i class="fa-brands fa-instagram"></i></div>
-                            <div class="info_sp">
-                                8k subscribe </div>
+                        </a>
+                        <a class="item" style="background:#890D0D" href="https://www.youtube.com/@sora.bridal252f" target="_blank" rel="noopener" aria-label="YouTube">
+                            <div class="icon"><i class="fa-brands fa-youtube"></i></div>
+                        </a>
+                        <a class="item" style="background:#BD081C" href="https://www.pinterest.com/sorabridal/" target="_blank" rel="noopener" aria-label="Pinterest">
+                            <div class="icon"><i class="fa-brands fa-pinterest-p"></i></div>
                         </a>
                     </div>
-                </div>
-                <div class="col-md-4 item_ft pay_ft">
-                    <div class="title_ft">Thanh toán</div>
-                    <div class="info_col">
-                        <span>TECHCOMBANK - TRAN THI HUYEN</span>
-                        <p>19036895971011 </p>
-                        <img class="lazyload codeqr" data-src="https://mimosawedding.vn/wp-content/uploads/2023/07/QR-Huyen.png" alt="">
+                    <div class="justify-content-end">
+                        <p class="mb-0 coppyright text-end">
+                            ©Sorabridal 2023. All Rights Reserved
+                        </p>
                     </div>
                 </div>
-                <div class="col-md-4 item_ft maps_ft">
-                    <div class="title_ft">Bản đồ</div>
-                    <div class="info_col">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7449.41904153903!2d105.816553!3d21.004278!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ac983b00855f%3A0x1f61520b0dd13411!2sMimosa%20Wedding%20Studio!5e0!3m2!1svi!2sus!4v1681118058543!5m2!1svi!2sus"
-                            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> </div>
-                </div>
             </div>
-            <div class="bot_ft d-flex justify-content-between">
-                <div class="more_web d-flex">
-                    <p class="mb-0">Sơ đồ </p>
-                    <ul class="list-unstyled d-flex mb-0">
-                        <li>
-                            <a href="https://mimosawedding.net/albums-anh" target="_blank">
-                            Album ảnh cưới                        </a>
-                        </li>
-                        <li>
-                            <a href="https://mimosawedding.net/goi-chup-anh-cuoi/" target="_blank">
-                            Gói chụp ảnh cưới                        </a>
-                        </li>
-                        <li>
-                            <a href="https://mimosawedding.net/vay-cuoi" target="_blank">
-                            Album váy cưới                        </a>
-                        </li>
-                        <li>
-                            <a href="https://mimosawedding.net/chup-anh-gia-dinh" target="_blank">
-                            Kinh nghiệm chụp ảnh gia đình                        </a>
-                        </li>
-                        <li>
-                            <a href="https://mimosawedding.net/kinh-nghiem-chup-anh-cuoi" target="_blank">
-                            Kinh nghiệm chụp ảnh cưới                        </a>
-                        </li>
-                    </ul>
-                </div>
-                <p class="mb-0 coppyright">
-                    ©Sorabridal 2023. All Rights Reserved </p>
-            </div>
+            
         </div>
     </div>
     <div class="cta_page" id="cta_support">
--- resources/views/users/partials/sections/film_set.blade.php
+++ resources/views/users/partials/sections/film_set.blade.php
@@ -1,41 +1,44 @@
 @php
-    $filmSets = [
-        [
-            'text' => 'Tầm nhìn panoramic, golden hour và ánh sáng tự nhiên — lý tưởng cho những khung hình cưới đầy cảm xúc giữa hoàng hôn Hà Nội.',
-            'img' => 'https://mimosawedding.vn/wp-content/uploads/2023/07/phim-truong-biet-thu-hoa-hong-rose-valley-32.jpg',
-            'name' => "Sora's Rooftop",
-            'role' => 'Tầng 8, 315 Bạch Mai, Hà Nội',
-        ],
-        [
-            'text' => 'Concept sân vườn, phong cách minimalist trong không gian giàu cảm xúc — nơi mỗi góc chụp đều kể câu chuyện tình yêu của riêng hai bạn.',
-            'img' => 'https://mimosawedding.vn/wp-content/uploads/2025/10/album-studio-han-quoc-2025-6.jpg',
-            'name' => "Sora's Mansion",
-            'role' => 'Hoàng Mai, Hà Nội',
-        ],
-    ];
+$filmSets = [
+[
+'text' => 'Tầm nhìn panoramic, golden hour và ánh sáng tự nhiên — lý tưởng cho những khung hình cưới đầy cảm xúc giữa
+hoàng hôn Hà Nội.',
+'img' => 'https://mimosawedding.vn/wp-content/uploads/2023/07/phim-truong-biet-thu-hoa-hong-rose-valley-32.jpg',
+'name' => "Sora's Rooftop",
+'role' => 'Tầng 8, 315 Bạch Mai, Hà Nội',
+],
+[
+'text' => 'Concept sân vườn, phong cách minimalist trong không gian giàu cảm xúc — nơi mỗi góc chụp đều kể câu chuyện
+tình yêu của riêng hai bạn.',
+'img' => 'https://mimosawedding.vn/wp-content/uploads/2025/10/album-studio-han-quoc-2025-6.jpg',
+'name' => "Sora's Mansion",
+'role' => 'Hoàng Mai, Hà Nội',
+],
+];
 @endphp
 
 <section id="section-film-set" class="py-5 overflow-hidden">
     <div class="container-xl">
         <div class="text-center text-uppercase mx-auto mb-5 col-lg-8 section-title-wrapper">
             <p class="section-title" data-aos="fade-up">Hệ sinh thái riêng</p>
-            <p class="section-description" data-aos="fade-up">Phim trường</p>
+            <p class="section-description" data-aos="fade-up">Phim trường & Concept độc quyền
+            </p>
         </div>
 
         <div class="row g-4" data-aos="fade-up">
             @foreach ($filmSets as $filmSet)
-                <div class="col-12 col-md-6">
-                    <div class="card border-0 bg-light h-100 text-center p-4">
-                        <img class="lazyload rounded mx-auto mb-3 object-fit-cover w-100" style="height: 320px;"
-                            data-src="{{ $filmSet['img'] }}" alt="{{ $filmSet['name'] }}">
-                        <p class="mb-4">{{ $filmSet['text'] }}</p>
-                        <div class="d-flex align-items-center justify-content-center flex-wrap gap-2 mb-0">
-                            <span class="fw-semibold">{{ $filmSet['name'] }}</span>
-                            <span class="text-muted" aria-hidden="true">·</span>
-                            <span class="small text-muted">{{ $filmSet['role'] }}</span>
-                        </div>
+            <div class="col-12 col-md-6">
+                <div class="card border-0 bg-light h-100 text-center p-4">
+                    <img class="lazyload rounded mx-auto mb-3 object-fit-cover w-100" style="height: 320px;"
+                        data-src="{{ $filmSet['img'] }}" alt="{{ $filmSet['name'] }}">
+                    <p class="mb-4">{{ $filmSet['text'] }}</p>
+                    <div class="d-flex align-items-center justify-content-center flex-wrap gap-2 mb-0">
+                        <span class="fw-semibold">{{ $filmSet['name'] }}</span>
+                        <span class="text-muted" aria-hidden="true">·</span>
+                        <span class="small text-muted">{{ $filmSet['role'] }}</span>
                     </div>
                 </div>
+            </div>
             @endforeach
         </div>
     </div>
